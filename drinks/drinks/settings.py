e5a75d270309dfac88dbded8dbe0234748f90cb1
Martin_Omollo <martinpati1952@gmail.com>
2023-04-03 10:16:59 +0300

    add post request

--- drinks/drinks/settings.py
+++ drinks/drinks/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'rest-framework',
+    'rest_framework',
     'drinks',
     'django.contrib.admin',
     'django.contrib.auth',
--- drinks/drinks/views.py
+++ drinks/drinks/views.py
@@ -1,14 +1,24 @@
 from django.http import JsonResponse
 from .models import Drink
-from .serializers import    DrinkSerializer
+from .serializers import DrinkSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response  import Response
+from rest_framework import status
 
+@api_view('GET', 'POST') 
+#Decorator something you put above your function to describe its functionality
 def drink_list(request):
     
     #1.get all the drinks
     #2.serialize them
     #3.return json
-
+ if request.method == 'GET':
    drinks =  Drink.objects.all() 
    serializer = DrinkSerializer(drinks, many = True)
    return JsonResponse({"drinks":serializer.data}, safe =  False)
-     
+ 
+ if request.method == 'POST':
+   serializer = DrinkSerializer(data=request.data)
+   if serializer.is_valid():
+     serializer.save() 
+     return Response(serializer.data, status=status.HTTP_201_CREATED)
